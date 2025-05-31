649c2fd3c04c6d42d7de362bcad1c990c26137aa
Hideaki Kondo <hdaknd@gmail.com>
2025-05-31 14:45:58 +0900

    bugfix

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -7,8 +7,11 @@ import { createClient } from "@supabase/supabase-js";
 import { type Database } from "../types/supabase"; // 生成された型定義をインポート
 import View from "./view";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+// Viteプロジェクトでは、環境変数は import.meta.env を通じてアクセスし、
+// デフォルトでは VITE_ というプレフィックスが必要です。
+// .env ファイルに VITE_SUPABASE_URL と VITE_SUPABASE_ANON_KEY が正しく定義されていることを確認してください。
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
 
 // createClientに関数を渡し、型情報を適用します
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
